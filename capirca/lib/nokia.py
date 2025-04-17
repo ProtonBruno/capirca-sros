085228d7fdf431d12431ada4803e69170e956276
Bruno Marin <bruno.marin@proton.ch>
2025-04-17 12:32:07 +0200

    fix: ipv6 any

--- capirca/lib/nokia.py
+++ capirca/lib/nokia.py
@@ -778,6 +778,8 @@ class Nokia(aclgenerator.ACLGenerator):
         # Nokia workaround for 0.0.0.0/0 in prefix list, expands to 2 /31s, IPv6 works with 2000::/3
         if str(ip) == "0.0.0.0/0":
           write_ips = [ "0.0.0.0/1", "128.0.0.0/1" ]
+        if str(ip) == "::0/0":
+          write_ips = [ "::/1", "8::/1" ]
         else:
           write_ips = [ str(ip) ]
 
